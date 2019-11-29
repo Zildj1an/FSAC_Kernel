6077bdaf371a583d0cdd3889a171113714598e4a
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2019-11-29 14:07:41 +0100

    Update fsac_example.c

--- fsac/fsac_example.c
+++ fsac/fsac_example.c
@@ -3,3 +3,19 @@
 */
 
 #include <fsac/fsac_plugin.h>
+
+/* Dummy plugin functions */
+
+static long fsac_dummy_activate_plugin(void) {
+	return 0;
+}
+
+static long fsac_dummy_deactivate_plugin(void) {
+	return 0;
+}
+
+struct fsac_plugin fsac_sched_plugin = {
+	.plugin_name = "FSAC",
+	.activate_plugin = ,
+	.deactivate_plugin = ,
+};
